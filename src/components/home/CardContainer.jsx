a2795eb55393ca648fd4612f5720fad0979557ad
Aizen-rz <lightnovachrono72@gmail.com>
2025-01-16 11:23:09 +0300

    Updated frontend: made changes in header section

--- src/components/home/CardContainer.jsx
+++ src/components/home/CardContainer.jsx
@@ -9,9 +9,9 @@ const CardContainer = ({ products }) => {
   };
 
   return (
-    <section className="py-5" id="shop">
-      <h4 style={{ textAlign: "center" }}>Our Products</h4>
-      <div className="container px-4 px-lg-5 mt-5">
+    <section className="py-3" id="shop"> {/* Reduced padding for the section */}
+      <h4 style={{ textAlign: "center", marginBottom: "1rem" }}>Our Products</h4> {/* Reduced margin-bottom */}
+      <div className="container px-4 px-lg-5 mt-3"> {/* Reduced margin-top */}
         <div className="row">
           {/* Pass the click handler to HomeCard */}
           {products.map((product) => (
